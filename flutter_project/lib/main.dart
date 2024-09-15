9f54cd2a4394631df80fb10f464c72088fcca271
foreverxxxxxx <emineogutlu3@gmail.com>
2024-09-15 14:26:46 +0300

    When you click on the Click button with the Text widget I created, the numbers on the screen increase.

--- flutter_project/lib/main.dart
+++ flutter_project/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const WidgetDetail());
+  runApp(const Test());
 }
 
 class WidgetDetail extends StatelessWidget {
@@ -23,6 +23,39 @@ class WidgetDetail extends StatelessWidget {
           onPressed: () {},
           child: const Text("+"),
         ),
+      ),
+    );
+  }
+}
+
+class Test extends StatefulWidget {
+  const Test({super.key});
+
+  @override
+  State<Test> createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  int value = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(value.toString()),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  value = value + 1;
+                });
+              },
+              child: const Text("Click"),
+            )
+          ],
+        )),
       ),
     );
   }
