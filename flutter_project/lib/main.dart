9a13450c8a188a848e2f344c94151cf2edfff2b5
foreverxxxxxx <emineogutlu3@gmail.com>
2024-09-15 15:12:56 +0300

    StatelessWidget was converted to StatefulWidget by clicking on the widget and pressing Ctrl+Shift+R.

--- flutter_project/lib/main.dart
+++ flutter_project/lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Test());
+  runApp(const WidgetDetail());
 }
 
-class WidgetDetail extends StatelessWidget {
+class WidgetDetail extends StatefulWidget {
   const WidgetDetail({super.key});
 
+  @override
+  State<WidgetDetail> createState() => _WidgetDetailState();
+}
+
+class _WidgetDetailState extends State<WidgetDetail> {
+  int value = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,46 +22,17 @@ class WidgetDetail extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.deepOrange,
         ),
-        body: const Center(
-          child: Text("Hello Flutter"),
+        body: Center(
+          child: Text(value.toString()),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            setState(() {
+              value = value + 1;
+            });
+          },
           child: const Text("+"),
         ),
-      ),
-    );
-  }
-}
-
-class Test extends StatefulWidget {
-  const Test({super.key});
-
-  @override
-  State<Test> createState() => _TestState();
-}
-
-class _TestState extends State<Test> {
-  int value = 0;
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(value.toString()),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  value = value + 1;
-                });
-              },
-              child: const Text("Click"),
-            )
-          ],
-        )),
       ),
     );
   }
