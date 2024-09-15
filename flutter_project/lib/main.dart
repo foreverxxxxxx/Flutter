c128f5e63b3b07505cced989722de61a10811586
foreverxxxxxx <emineogutlu3@gmail.com>
2024-09-15 12:50:41 +0300

    In the Dart code, title, centerTitle, backgroundColour and lead, which are the properties of the appBar inside the Scaffold widget, were used.

---
+++ flutter_project/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WidgetDetail());
+}
+
+class WidgetDetail extends StatelessWidget {
+  const WidgetDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Widget Detail"),
+          centerTitle: true,
+          backgroundColor: Colors.deepOrange,
+        ),
+      ),
+    );
+  }
+}
